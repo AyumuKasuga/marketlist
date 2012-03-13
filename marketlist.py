ac8a58e11177b2dec337f8848d1aab7a5b5c61fb
AyumuKasuga <bps@dzen.eu>
2012-03-14 01:04:15 +0400

    теперь все добавляется в базу

--- marketlist.py
+++ marketlist.py
@@ -1,11 +1,13 @@
 # coding: utf-8
+import re
 
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
+from google.appengine.ext import db
 
-from model import NoteIndex
+from model import NoteIndex, NoteList
 
 def user_bar(page):
     user = users.get_current_user()
@@ -28,10 +30,31 @@ class MainPage(webapp.RequestHandler):
         self.redirect('/')
 
 class AddPage(webapp.RequestHandler):
+    def items_to_insert(self, x):
+        if re.match("item-(\d+)", x):
+            value = self.request.get(x)
+            if value is not u'':
+                r = re.match("(.*)\[(\d+)\]", value)
+                if r is not None:
+                    price = r.groups()[1]
+                    value = r.groups()[0]
+                else:
+                    price = 0
+                self.items.append({'name':value, 'price':price})
+
     def get(self):
         self.response.out.write(template.render('templates/addform.html', {'userbar':user_bar(page = self.request.uri)}))
     def post(self):
-        print self.request.arguments()
+        arguments = self.request.arguments()
+        title = self.request.get('title')
+        self.items = []
+        print 1
+        map(self.items_to_insert, arguments)
+        n = NoteIndex(user=users.get_current_user(), title=title)
+        n.put()
+        inserts = []
+        map(lambda x: inserts.append(NoteList(noteindex = n, name = x['name'], price = int(x['price']))), self.items)
+        db.put(inserts)
     
 urls = [
     ('/', MainPage),
--- model.py
+++ model.py
@@ -1,4 +1,12 @@
 from google.appengine.ext import db
 
 class NoteIndex(db.Model):
-    title = db.StringProperty()
+    user = db.UserProperty(required=True)
+    date = db.DateTimeProperty(auto_now_add=True, required=True)
+    title = db.StringProperty(required=True)
+    
+class NoteList(db.Model):
+    noteindex = db.ReferenceProperty(NoteIndex)
+    name = db.StringProperty(required=True)
+    price = db.IntegerProperty()
+    
